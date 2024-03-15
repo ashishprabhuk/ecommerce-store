de3d04c2fbb72c0841fd7ed91e41f87557d00aea
Ashish Prabhu <ashishprabhu2143@gmail.com>
2024-03-15 13:07:16 +0000

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<SignUp/>}/>
+        <Route path='/signup' element={<Login/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
     </>
   )
 }
